6b4f3b05ce0e66371cbbc3f4d3c6e0b4c5266642
Flowsqy <Flo504.Dev@gmail.com>
2021-11-14 14:02:27 +0100

    Init project

---
+++ src/main/java/fr/flowqsy/petownerchecker/PetOwnerCheckerPlugin.java
@@ -0,0 +1,12 @@
+package fr.flowqsy.petownerchecker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PetOwnerCheckerPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
